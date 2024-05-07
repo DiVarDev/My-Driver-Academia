531f54423cc0739b86eab6da2e71cf74f3c33e26
DiVarDev <118238723+DiVarDev@users.noreply.github.com>
2024-05-07 00:41:17 -0500

    Worked on the main menu animations and timeline

--- Assets/Scripts/Player/CarMovement.cs
+++ Assets/Scripts/Player/CarMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,32 +10,57 @@ public class CarMovement : MonoBehaviour
 {
     // Variables
     [Header("RigidBody and Player Velocity")]
-    [SerializeField] private Rigidbody carRigidBody;
-    [SerializeField] private Vector3 carVelocity;
-    [SerializeField] private Vector3 forwardVelocity;
-    [SerializeField] private Vector3 wheelTurnValue;
-    [Header("Attributes")]
-    [Range(1f, 100f)]
-    [SerializeField] private float speed = 25.0f;
-    [Range(1f, 10f)]
-    [SerializeField] private float multiplier = 5.0f;
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private float gravity = -9.8f;
-    [Header("Car Parts")]
-    [SerializeField] private GameObject frontRightWheel;
-    [SerializeField] private GameObject frontLeftWheel;
-    [SerializeField] private GameObject rearRightWheel;
-    [SerializeField] private GameObject rearLeftWheel;
-    [SerializeField] private Vector3 innerWheelRotationMax = new Vector3(0.0f, 0.0f, 0.0f);
-    [SerializeField] private Vector3 outterWheelRotationMax = new Vector3(0.0f, 0.0f, 0.0f);
-    [Header("Race Stats")]
-    [SerializeField] private int lapsCount;
-    [SerializeField] private TMP_Text lapsCountText;
+    public Rigidbody carRigidBody;
+    public Vector3 carVelocity;
+    public Vector3 forwardVelocity;
+    public Vector3 wheelTurnValue;
+
+    [Header("Statistics")]
+    [Range(20.0f, 190.0f)]
+    public float maxSpeed = 90.0f; //The maximum speed that the car can reach in km/h.
+    [Range(10.0f, 120.0f)]
+    public float maxReverseSpeed = 45.0f; //The maximum speed that the car can reach while going on reverse in km/h.
+    [Range(1.0f, 10.0f)]
+    public float accelerationMultiplier = 2.0f; // How fast the car can accelerate. 1 is a slow acceleration and 10 is the fastest.
+    [Range(10.0f, 45.0f)]
+    public float maxSteeringAngle = 27.0f; // The maximum angle that the tires can reach while rotating the steering wheel.
+    [Range(0.1f, 1f)]
+    public float steeringSpeed = 0.5f; // How fast the steering wheel turns.
+    [Range(100.0f, 600.0f)]
+    public float brakeForce = 350.0f; // The strength of the wheel brakes.
+    [Range(1.0f, 10.0f)]
+    public float decelerationMultiplier = 2.0f; // How fast the car decelerates when the user is not using the throttle.
+    [Range(1.0f, 10.0f)]
+    public float handbrakeDriftMultiplier = 5.0f; // How much grip the car loses when the user hit the handbrake.
+
+    [Header("Parts Meshes and Colliders")]
+    public GameObject frontRightWheelMesh;
+    public WheelCollider frontRightWheelCollider;
+    public GameObject frontLeftWheelMesh;
+    public WheelCollider frontLeftWheelCollider;
+    public GameObject rearRightWheelMesh;
+    public WheelCollider rearRightWheelCollider;
+    public GameObject rearLeftWheelMesh;
+    public WheelCollider rearLeftWheelCollider;
+
+    [Header("Sound")]
+    public AudioSource carAudioSource;
+
+    [Header("Other Statistics")]
+    public int lapsCount;
+    public TMP_Text lapsCountText;
+    public int carSpeed;
+    public TMP_Text speedText;
+    public int carFuelLeft;
+    public TMP_Text fuelText;
 
     // Start is called before the first frame update
     void Start()
     {
-        carRigidBody = GetComponent<Rigidbody>();
+        if (carRigidBody == null)
+        {
+            carRigidBody = GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
@@ -58,7 +84,7 @@ public class CarMovement : MonoBehaviour
             playervelocity.y = gravity;
         }*/
         //controller.Move(playervelocity * Time.deltaTime);
-        carRigidBody.AddForce(forwardVelocity * (speed * multiplier) * Time.deltaTime);
+        //carRigidBody.AddForce(forwardVelocity * (speed * multiplier) * Time.deltaTime);
         //Debug.Log(forwardVelocity);
     }
 
@@ -80,7 +106,7 @@ public class CarMovement : MonoBehaviour
             playervelocity.y = gravity;
         }*/
         //controller.Move(playervelocity * Time.deltaTime);
-        carRigidBody.AddForce(wheelTurnValue * (speed * multiplier) * Time.deltaTime);
+        //carRigidBody.AddForce(wheelTurnValue * (speed * multiplier) * Time.deltaTime);
         //Debug.Log(wheelTurnValue);
     }
 }
